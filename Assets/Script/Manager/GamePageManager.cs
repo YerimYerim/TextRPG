4fc44c1ae0df6b6119e78fb3d64f757b0e18078f
YerimYerim <vigvang_bip@naver.com>
2023-10-14 15:25:35 +0900

    [yerim] json 폴더 변경및 로드 방식 변경

--- Assets/Script/Manager/GamePageManager.cs
+++ Assets/Script/Manager/GamePageManager.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Script.DataClass;
-using Unity.VisualScripting;
-using UnityEngine;
 using Random = UnityEngine.Random;
 
 
